26acdeed91657b8d876aed56643a88fbfa08a496
李洪健 <lihongjian@izaodao.com>
2021-03-16 13:45:06 +0800

    Aop

---
+++ base-service/src/main/java/com/lhj/baseservice/aop/MyAspect.java
@@ -0,0 +1,76 @@
+package com.lhj.baseservice.aop;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class MyAspect {
+    @Pointcut("execution(public * com.lhj.baseservice.module.*.controller..*.*(..))")//切入点描述 这个是controller包的切入点
+    public void controllerLog() {
+    }//签名，可以理解成这个切入点的一个名称
+
+    @Before("controllerLog()")
+    public void logBeforeController(JoinPoint joinPoint) {
+        log.error("------->before");
+
+        //这个RequestContextHolder是Springmvc提供来获得请求的东西
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+
+        // 记录下请求内容
+        log.info("################URL : " + request.getRequestURL().toString());
+        log.info("################HTTP_METHOD : " + request.getMethod());
+        log.info("################IP : " + request.getRemoteAddr());
+        log.info("################THE ARGS OF THE CONTROLLER : " + Arrays.toString(joinPoint.getArgs()));
+
+        //下面这个getSignature().getDeclaringTypeName()是获取包+类名的   然后后面的joinPoint.getSignature.getName()获取了方法名
+        log.info("################CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+
+    }
+
+    @After("controllerLog()")
+    public void logAfterController(JoinPoint joinPoint) {
+        log.error("------->After");
+        //这个RequestContextHolder是Springmvc提供来获得请求的东西
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+
+        // 记录下请求内容
+        log.info("################URL : " + request.getRequestURL().toString());
+        log.info("################HTTP_METHOD : " + request.getMethod());
+        log.info("################IP : " + request.getRemoteAddr());
+        log.info("################THE ARGS OF THE CONTROLLER : " + Arrays.toString(joinPoint.getArgs()));
+
+        //下面这个getSignature().getDeclaringTypeName()是获取包+类名的   然后后面的joinPoint.getSignature.getName()获取了方法名
+        log.info("################CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+    }
+
+    @Around("controllerLog()")
+    public void logAroundController(JoinPoint joinPoint) {
+        log.error("------->Around");
+
+        //这个RequestContextHolder是Springmvc提供来获得请求的东西
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+
+        // 记录下请求内容
+        log.info("################URL : " + request.getRequestURL().toString());
+        log.info("################HTTP_METHOD : " + request.getMethod());
+        log.info("################IP : " + request.getRemoteAddr());
+        log.info("################THE ARGS OF THE CONTROLLER : " + Arrays.toString(joinPoint.getArgs()));
+
+        //下面这个getSignature().getDeclaringTypeName()是获取包+类名的   然后后面的joinPoint.getSignature.getName()获取了方法名
+        log.info("################CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+    }
+}
